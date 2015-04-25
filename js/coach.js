147c19b21e700250c794031ae5621445b138f44f
零一 <li.lli@alibaba-inc.com>
2015-04-25 11:33:59 +0800

    列表

--- js/coach.js
+++ js/coach.js
@@ -3,15 +3,17 @@
  */
 $(function(){
     var coachUrl = "mock/coach.json";
-    function getCoachList($scope,$http) {
+    angular.module("fire", []).controller("getCoachList",function getCoachList($scope,$http) {
         $http.get(coachUrl).success(function (response) {
-            if(response.errCode != 0){
+            if(response.errCode){
                 //提示错误
                 return;
             }
+            console.log($scope);
             $scope.coaches = response;
         }).error(function(data,state){
             //提示错误
         });
-    }
+    });
+
 }());
