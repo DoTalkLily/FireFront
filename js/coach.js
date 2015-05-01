cef13417075a3c9e00e4df8b2369c45db946283c
零一 <li.lli@alibaba-inc.com>
2015-05-01 14:43:45 +0800

    register done

--- js/coach.js
+++ js/coach.js
@@ -10,14 +10,14 @@ $(function () {
     /** 教练列表 **/
     app.controller("getCoachList", function ($scope, $http) {
         $http.get(COACH_LIST_URL).success(function (response) {
-            if (response.errCode) {
-                //错误提示
+            if (response.error_code) {
+                new Toast({context:$('body'),message:result.error}).show();
                 return;
             }
             $("#coach-list").css("display", "block");
             $scope.coaches = response;
         }).error(function (data, state) {
-            //错误提示
+            new Toast({context:$('body'),message:"系统错误，请稍后再试"}).show();
         });
         $scope.getGender = function (genderType) {
             return genderType == 1 ? "男" : "女";
@@ -37,21 +37,21 @@ $(function () {
             return genderType == 1 ? "男" : "女";
         }
         $http.get(COACH_DETAIL_URL, {params: {"id": id}}).success(function (response) {
-            if (response.errCode) {
-                //错误提示
+            if (response.error_code) {
+                new Toast({context:$('body'),message:result.error}).show();
                 return;
             }
             $("#coach-detail").css("display", "block");
             $scope.coach = response;
         }).error(function (data, state) {
-            //错误提示
+            new Toast({context:$('body'),message:"系统错误，请稍后再试"}).show();
         });
     });
 
     function submitOrder() {
         var orderChecked = $('input:checkbox[name="sport"]:checked');
         if (orderChecked.length == 0) {
-            showTips();
+            new Toast({context:$('body'),message:"请选择项目"}).show();
             return false;
         }
         var orderIdArr = [], i = 0, len = orderChecked.length;
--- js/common/utils.js
+++ js/common/utils.js
@@ -5,5 +5,44 @@
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-    if (r != null) return unescape(r[2]); return null; //返回参数值
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
+}
+/**
+ * 模仿android里面的Toast效果，主要是用于在不打断程序正常执行的情况下显示提示数据
+ * @param config
+ * @return
+ */
+var Toast = function(config){
+    this.msgEntity;
+    this.context = config.context==null?$('body'):config.context;//上下文
+    this.message = config.message;//显示内容
+    this.time = config.time==null?3000:config.time;//持续时间
+    this.left = config.left;//距容器左边的距离
+    this.top = config.top;//距容器上方的距离
+    this.init();
+}
+
+Toast.prototype = {
+    //初始化显示的位置内容等
+    init : function(){
+        $("#toastMessage").remove();
+        //设置消息体
+        var msgDIV = new Array();
+        msgDIV.push('<div id="toastMessage" style="border-radius: 4px">');
+        msgDIV.push('<span>'+this.message+'</span>');
+        msgDIV.push('</div>');
+        this.msgEntity = $(msgDIV.join('')).appendTo(this.context);
+        //设置消息样式
+        var left = this.left == null ? this.context.width()/2-this.msgEntity.find('span').width()/2 : this.left;
+        var top = this.top == null ? this.context.height()/2-this.msgEntity.find('span').height()/2  : this.top;
+        this.msgEntity.css({position:'absolute',top:top,'z-index':'99',left:left,'background-color':'black',color:'white','font-size':'18px',padding:'10px',margin:'10px'});
+        this.msgEntity.hide();
+    },
+    //显示动画
+    show :function(){
+        this.msgEntity.fadeIn(this.time/2);
+        this.msgEntity.fadeOut(this.time/2);
+    }
+
 }
